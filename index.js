db050277675ec6b9fc9736f0722b92c38dddb23a
root <ngohuynhngockhanh@gmail.com>
2017-02-27 20:40:15 +0700

    ok

--- index.js
+++ index.js
@@ -9,33 +9,31 @@ var io = socketio(app);								//#Phải khởi tạo io sau khi tạo app!
 app.listen(PORT);										// Cho socket server (chương trình mạng) lắng nghe ở port 3484
 console.log("Server nodejs chay tai dia chi: " + ip.address() + ":" + PORT)
 
-//giải nén chuỗi JSON thành các OBJECT
-function ParseJson(jsondata) {
-    try {
-        return JSON.parse(jsondata);
-    } catch (error) {
-        return null;
-    }
-}
-
 //Khi có mệt kết nối được tạo giữa Socket Client và Socket Server
 io.on('connection', function(socket) {	
 	//hàm console.log giống như hàm Serial.println trên Arduino
     console.log("Connected"); //In ra màn hình console là đã có một Socket Client kết nối thành công.
 	
-	var led = [true, false]
+	var led = [true, false] //định nghĩa một mảng 1 chiều có 2 phần tử: true, false. Mảng này sẽ được gửi đi nhằm thay đổi sự sáng tắt của 2 con đèn LED đỏ và xanh. Dựa vào cài đặt ở Arduino mà đèn LEd sẽ bị bật hoặc tắt. Hãy thử tăng hoạt giảm số lượng biến của mảng led này xem. Và bạn sẽ hiểu điều kỳ diệu của JSON!
+	
+	//Tạo một chu kỳ nhiệm vụ sẽ chạy lại sau mỗi 200ms
 	var interval1 = setInterval(function() {
+		//đảo trạng thái của mảng led, đảo cho vui để ở Arduino nó nhấp nháy cho vui.
 		for (var i = 0; i < led.length; i++) {
 			led[i] = !led[i]
 		}
+		
+		//Cài đặt chuỗi JSON, tên biến JSON này là json 
 		var json = {
-			led: led
+			"led": led //có một phần tử là "led", phần tử này chứa giá trị của mảng led.
 		}
-		socket.emit('LED', json)
-		console.log("send LED")
-	}, 100)
+		socket.emit('LED', json) //Gửi lệnh LED với các tham số của của chuỗi JSON
+		console.log("send LED")//Ghi ra console.log là đã gửi lệnh LED
+	}, 200)//200ms
+	
+	//Khi socket client bị mất kết nối thì chạy hàm sau.
 	socket.on('disconnect', function() {
-		console.log("disconnect")
-		clearInterval(interval1)
+		console.log("disconnect") 	//in ra màn hình console cho vui
+		clearInterval(interval1)		//xóa chu kỳ nhiệm vụ đi, chứ không xóa là cái task kia cứ chạy mãi thôi đó!
 	})
 });
